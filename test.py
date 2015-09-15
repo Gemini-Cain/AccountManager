339ebee137f92a0a4f28e7842c8a815c97fbf31a
duxin <duxin@bestpay.com.cn>
2015-09-15 21:14:59 +0800

    json解析测试

---
+++ test.py
@@ -0,0 +1,27 @@
+﻿#@Date 2015/09/15
+#@Author Xin Du
+#coding:utf-8
+
+import json
+
+class Json(object):
+	"""Json处理"""
+	def __init__(self, file):
+		super(Json, self).__init__()
+		self.file = file
+	
+	def readJson(self):
+		for line in open(self.file):
+			#line = line.decode("gbk").encode("utf-8")
+			print line.decode("utf-8").encode("gbk")
+			text = json.loads(line)
+			for key in text.keys():
+				print "[" + key + "]"
+				print text[key]
+ 	
+def test():
+	json = Json("D:\\Code\\AccountManager\\test.txt")
+	json.readJson()
+
+if __name__ == '__main__':
+	test()
