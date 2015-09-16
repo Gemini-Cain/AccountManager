ca86246bd7969cc47cae996ffca1de4e6774ae00
duxin <duxin@bestpay.com.cn>
2015-09-16 22:32:07 +0800

    json测试

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import json
 class DataHandler(object):
 	"""Json处理"""
 	def __init__(self, file):
-		super(Json, self).__init__()
+		super(DataHandler, self).__init__()
 		self.file = file
 	
 	def __readJsonFile__(self):
@@ -33,19 +33,28 @@ class DataHandler(object):
 				print text[key]
 
 	def search(self, condition):
-		result = null
+		result = None
 		flag = True
 		for line in open(self.file):
+			flag = True
 			#print line.decode("utf-8").encode("gbk")
+			line = line.decode("utf-8").encode("utf-8")
 			text = json.loads(line)
 			for key in condition.keys():
+				print key
+				if key in text.keys():
+					print text[key]
+				else:
+					flag = False
+					break
+				print condition[key]
 				if text[key] == condition[key]:
 					pass
 				else:
 					flag = False
 					break
 			if flag == True:
-				result = line
+				result = text
 				break
 
 		return result
@@ -53,7 +62,7 @@ class DataHandler(object):
 	def insert(self, key, content):
 		for line in open(self.file):
 			#line = line.decode("gbk").encode("utf-8")
-			print line.decode("utf-8").encode("gbk")
+			print line.decode("utf-8").encode("utf-8")
 			text = json.loads(line)
 			for key in text.keys():
 				if text[key]:
@@ -62,34 +71,10 @@ class DataHandler(object):
 				print text[key]
 
 def test():
-	json = Json("D:\\Code\\AccountManager\\test.txt")
-	json.readJson()
-
-if __name__ == '__main__':
-	test()#@Date 2015/09/15
-#@Author Xin Du
-#coding:utf-8
-
-import json
-
-class Json(object):
-	"""Json处理"""
-	def __init__(self, file):
-		super(Json, self).__init__()
-		self.file = file
-	
-	def readJson(self):
-		for line in open(self.file):
-			#line = line.decode("gbk").encode("utf-8")
-			print line.decode("utf-8").encode("gbk")
-			text = json.loads(line)
-			for key in text.keys():
-				print "[" + key + "]"
-				print text[key]
- 	
-def test():
-	json = Json("D:\\Code\\AccountManager\\test.txt")
-	json.readJson()
+	json = DataHandler("D:\\Code\\AccountManager\\test.txt")
+	condition = {"name" : str("豆瓣").decode("utf-8"), "symbol" : str("DB").decode("utf-8")}
+	result = json.search(condition)
+	print result
 
 if __name__ == '__main__':
 	test()
